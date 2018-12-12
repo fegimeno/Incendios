ed297a681ccf237f02190409b04ba658b2e54370
fegimeno <45392773+fegimeno@users.noreply.github.com>
2018-12-12 15:21:54 -0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -22,7 +22,7 @@ navbarPage("Comuna", id="nav",
                         ),
                         
                         # If not using custom CSS, set height of leafletOutput to a number instead of percent
-                        leafletOutput("map", width="100%", height="100%"),
+                        leafletOutput("mymap", width="100%", height="100%"),
                         
                         # Shiny versions prior to 0.11 should use class = "modal" instead.
                         absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
