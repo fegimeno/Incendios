c69ba2e6b0cb82590500dc454adbaf824c5edc90
fegimeno <45392773+fegimeno@users.noreply.github.com>
2018-12-12 10:27:26 -0300

    Update server.R

--- server.R
+++ server.R
@@ -6,10 +6,10 @@ library(dplyr)
 
 # Leaflet bindings are a bit slow; for now we'll just sample to compensate
 set.seed(100)
-zipdata <- allzips[sample.int(nrow(allzips), 10000),]
+zipdata <- allzips[sample.int(nrow(allzips), 100),]
 # By ordering by centile, we ensure that the (comparatively rare) SuperZIPs
 # will be drawn last and thus be easier to see
-zipdata <- zipdata[order(zipdata$centile),]
+zipdata <- zipdata[order(zipdata$rol),]
 
 function(input, output, session) {
   
@@ -35,34 +35,34 @@ function(input, output, session) {
     lngRng <- range(bounds$east, bounds$west)
     
     subset(zipdata,
-           latitude >= latRng[1] & latitude <= latRng[2] &
-             longitude >= lngRng[1] & longitude <= lngRng[2])
+           LAT >= latRng[1] & LAT <= latRng[2] &
+             LONG >= lngRng[1] & LONG <= lngRng[2])
   })
   
   # Precalculate the breaks we'll need for the two histograms
-  centileBreaks <- hist(plot = FALSE, allzips$centile, breaks = 20)$breaks
+#  centileBreaks <- hist(plot = FALSE, allzips$centile, breaks = 20)$breaks
   
-  output$histCentile <- renderPlot({
+#  output$histCentile <- renderPlot({
     # If no zipcodes are in view, don't plot
-    if (nrow(zipsInBounds()) == 0)
+#    if (nrow(zipsInBounds()) == 0)
       return(NULL)
     
-    hist(zipsInBounds()$centile,
-         breaks = centileBreaks,
-         main = "SuperZIP score (visible zips)",
-         xlab = "Percentile",
-         xlim = range(allzips$centile),
-         col = '#00DD00',
-         border = 'white')
-  })
+#    hist(zipsInBounds()$centile,
+#         breaks = centileBreaks,
+#         main = "SuperZIP score (visible zips)",
+#         xlab = "Percentile",
+#         xlim = range(allzips$centile),
+#         col = '#00DD00',
+#         border = 'white')
+#  })
   
-  output$scatterCollegeIncome <- renderPlot({
+#  output$scatterCollegeIncome <- renderPlot({
     # If no zipcodes are in view, don't plot
-    if (nrow(zipsInBounds()) == 0)
-      return(NULL)
+#    if (nrow(zipsInBounds()) == 0)
+#      return(NULL)
     
-    print(xyplot(income ~ college, data = zipsInBounds(), xlim = range(allzips$college), ylim = range(allzips$income)))
-  })
+#    print(xyplot(income ~ college, data = zipsInBounds(), xlim = range(allzips$college), ylim = range(allzips$income)))
+#  })
   
   # This observer is responsible for maintaining the circles and legend,
   # according to the variables the user has chosen to map to color and size.
@@ -73,7 +73,7 @@ function(input, output, session) {
     if (colorBy == "superzip") {
       # Color and palette are treated specially in the "superzip" case, because
       # the values are categorical instead of continuous.
-      colorData <- ifelse(zipdata$centile >= (100 - input$threshold), "yes", "no")
+      #colorData <- ifelse(zipdata$rol >= (100 - input$threshold), "yes", "no")
       pal <- colorFactor("viridis", colorData)
     } else {
       colorData <- zipdata[[colorBy]]
@@ -96,88 +96,88 @@ function(input, output, session) {
   })
   
   # Show a popup at the given location
-  showZipcodePopup <- function(zipcode, lat, lng) {
-    selectedZip <- allzips[allzips$zipcode == zipcode,]
-    content <- as.character(tagList(
-      tags$h4("Score:", as.integer(selectedZip$centile)),
-      tags$strong(HTML(sprintf("%s, %s %s",
-                               selectedZip$city.x, selectedZip$state.x, selectedZip$zipcode
-      ))), tags$br(),
-      sprintf("Median household income: %s", dollar(selectedZip$income * 1000)), tags$br(),
-      sprintf("Percent of adults with BA: %s%%", as.integer(selectedZip$college)), tags$br(),
-      sprintf("Adult population: %s", selectedZip$adultpop)
-    ))
-    leafletProxy("map") %>% addPopups(lng, lat, content, layerId = zipcode)
-  }
+#  showZipcodePopup <- function(zipcode, lat, lng) {
+#    selectedZip <- allzips[allzips$rol == rol,]
+#    content <- as.character(tagList(
+#      tags$h4("Score:", as.integer(selectedZip$año)),
+#      tags$strong(HTML(sprintf("%s, %s %s",
+#                               selectedZip$NOM_COM, selectedZip$NOM_REG, selectedZip$rol
+#      ))), tags$br(),
+      #sprintf("Median household income: %s", dollar(selectedZip$income * 1000)), tags$br(),
+      #sprintf("Percent of adults with BA: %s%%", as.integer(selectedZip$college)), tags$br(),
+      #sprintf("Adult population: %s", selectedZip$adultpop)
+#    ))
+#    leafletProxy("map") %>% addPopups(lng, lat, content, layerId = )
+#  }
   
   # When map is clicked, show a popup with city info
-  observe({
-    leafletProxy("map") %>% clearPopups()
-    event <- input$map_shape_click
-    if (is.null(event))
-      return()
+#  observe({
+#    leafletProxy("map") %>% clearPopups()
+#    event <- input$map_shape_click
+#    if (is.null(event))
+#      return()
     
-    isolate({
-      showZipcodePopup(event$id, event$lat, event$lng)
-    })
-  })
+#    isolate({
+#      showZipcodePopup(event$id, event$lat, event$lng)
+#    })
+#  })
   
   
   ## Data Explorer ###########################################
   
-  observe({
-    cities <- if (is.null(input$states)) character(0) else {
-      filter(cleantable, State %in% input$states) %>%
-        `$`('City') %>%
-        unique() %>%
-        sort()
-    }
-    stillSelected <- isolate(input$cities[input$cities %in% cities])
-    updateSelectInput(session, "cities", choices = cities,
-                      selected = stillSelected)
-  })
+#  observe({
+#    cities <- if (is.null(input$states)) character(0) else {
+#      filter(cleantable, State %in% input$states) %>%
+#        `$`('City') %>%
+#        unique() %>%
+#        sort()
+#    }
+#    stillSelected <- isolate(input$cities[input$cities %in% cities])
+#    updateSelectInput(session, "cities", choices = cities,
+#                      selected = stillSelected)
+#  })
   
-  observe({
-    zipcodes <- if (is.null(input$states)) character(0) else {
-      cleantable %>%
-        filter(State %in% input$states,
-               is.null(input$cities) | City %in% input$cities) %>%
-        `$`('Zipcode') %>%
-        unique() %>%
-        sort()
-    }
-    stillSelected <- isolate(input$zipcodes[input$zipcodes %in% zipcodes])
-    updateSelectInput(session, "zipcodes", choices = zipcodes,
-                      selected = stillSelected)
-  })
+#  observe({
+#    zipcodes <- if (is.null(input$states)) character(0) else {
+#      cleantable %>%
+#        filter(State %in% input$states,
+#               is.null(input$cities) | City %in% input$cities) %>%
+#        `$`('Zipcode') %>%
+#        unique() %>%
+#        sort()
+#    }
+#    stillSelected <- isolate(input$zipcodes[input$zipcodes %in% zipcodes])
+#    updateSelectInput(session, "zipcodes", choices = zipcodes,
+#                      selected = stillSelected)
+#  })
   
-  observe({
-    if (is.null(input$goto))
-      return()
-    isolate({
-      map <- leafletProxy("map")
-      map %>% clearPopups()
-      dist <- 0.5
-      zip <- input$goto$zip
-      lat <- input$goto$lat
-      lng <- input$goto$lng
-      showZipcodePopup(zip, lat, lng)
-      map %>% fitBounds(lng - dist, lat - dist, lng + dist, lat + dist)
-    })
-  })
+#  observe({
+#    if (is.null(input$goto))
+#      return()
+#    isolate({
+#      map <- leafletProxy("map")
+#      map %>% clearPopups()
+#      dist <- 0.5
+#      zip <- input$goto$zip
+#      lat <- input$goto$lat
+#      lng <- input$goto$lng
+#      showZipcodePopup(zip, lat, lng)
+#      map %>% fitBounds(lng - dist, lat - dist, lng + dist, lat + dist)
+#    })
+#  })
   
-  output$ziptable <- DT::renderDataTable({
-    df <- cleantable %>%
-      filter(
-        Score >= input$minScore,
-        Score <= input$maxScore,
-        is.null(input$states) | State %in% input$states,
-        is.null(input$cities) | City %in% input$cities,
-        is.null(input$zipcodes) | Zipcode %in% input$zipcodes
-      ) %>%
-      mutate(Action = paste('<a class="go-map" href="" data-lat="', Lat, '" data-long="', Long, '" data-zip="', Zipcode, '"><i class="fa fa-crosshairs"></i></a>', sep=""))
-    action <- DT::dataTableAjax(session, df)
+#  output$ziptable <- DT::renderDataTable({
+#    df <- cleantable %>%
+#      filter(
+#        Score >= input$minScore,
+#        Score <= input$maxScore,
+#        is.null(input$states) | State %in% input$states,
+#        is.null(input$cities) | City %in% input$cities,
+#        is.null(input$zipcodes) | Zipcode %in% input$zipcodes
+#      ) %>%
+#      mutate(Action = paste('<a class="go-map" href="" data-lat="', Lat, '" data-long="', Long, '" data-zip="', Zipcode, '"><i class="fa fa-crosshairs"></i></a>', sep=""))
+#    action <- DT::dataTableAjax(session, df)
     
-    DT::datatable(df, options = list(ajax = list(url = action)), escape = FALSE)
-  })
-}
+#    DT::datatable(df, options = list(ajax = list(url = action)), escape = FALSE)
+#  })
+#}
